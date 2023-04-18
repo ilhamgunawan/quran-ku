e7631fa9693758806da0257221d9bb8b2ac146c7
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-04-18 13:01:10 +0700

    feat: refactor not found page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
-import SadIcon from '../assets/icons/sad-icon';
-import Header from '../components/header/Header';
+import Header from '@/components/header/Header';
+import SadIcon from '@/components/SadIcon';
 
 export default function NotFoundPage() {
   return (
@@ -10,10 +10,7 @@ export default function NotFoundPage() {
         <title>Halaman tidak ditemukan | QuranKu</title>
       </Head>
       <Header pageTitle="QuranKu" />
-      <main
-        style={{ width: '95%', marginLeft: 'auto', marginRight: 'auto' }}
-        className="my-16 flex flex-col items-center"
-      >
+      <main className="mx-auto my-28 flex w-11/12 flex-col items-center gap-4">
         <h2 className="text-6xl font-bold">404</h2>
         <p className="text-center text-xl">
           Sayang sekali, halaman yang anda minta tidak ditemukan <SadIcon />
