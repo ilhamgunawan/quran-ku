08547155abacbd3488216aa5309f8d1ebaa6ec28
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2023-04-20 17:13:26 +0700

    feat: refactor sudah detail page

--- pages/surat/[id].tsx
+++ pages/surat/[id].tsx
@@ -1,24 +1,14 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 import type { GetStaticProps } from 'next';
-import Head from 'next/head';
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 
-import AyatListItem from '../../components/ayat-list/ayat-list-item';
-import Footer from '../../components/footer/footer';
-import Header from '../../components/header/Header';
+import SurahDetailApp from '@/apps/SurahDetail';
+import type { ISurahDetail } from '@/types/surah';
+
 import {
   getAllSurahId,
   getMurottalUrl,
   getSurah,
-  getSurahNameAndAyat,
 } from '../../data-sources/data-sources';
-import {
-  murottalAudioToggle,
-  setCurrentAyatNumberList,
-  setCurrentMurottal,
-} from '../../state/actions';
-import { DispatchContext, GlobalContext } from '../../state/Store';
-import { getAyatNumberList, mapAyatObjectToArray } from '../../utils/utils';
 
 export const getStaticPaths = async () => {
   const paths = await getAllSurahId();
@@ -29,120 +19,24 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { nextName, previousName } = await getSurahNameAndAyat(params?.id);
   const murottalUrl = await getMurottalUrl(params?.id);
   const surahData = await getSurah(params?.id);
 
   return {
     props: {
       surahData,
-      surahId: params?.id,
-      nextName,
-      previousName,
       murottalUrl,
     },
   };
 };
 
-interface SurahDetailProps {
-  surahData: any;
-  surahId: any;
+interface SurahDetailPageProps {
+  surahData: ISurahDetail;
   murottalUrl: any;
-  nextName: any;
-  previousName: any;
 }
 
-const SurahDetail = ({
-  surahData,
-  surahId,
-  murottalUrl,
-  nextName,
-  previousName,
-}: SurahDetailProps) => {
-  const state = useContext(GlobalContext);
-  const dispatch = useContext(DispatchContext);
-  const { currentMurottal, isMurottalPlaying } = state;
-
-  const { text, translations, number, name_latin, number_of_ayah } = surahData;
-
-  const versesArray = mapAyatObjectToArray(text, translations.id.text);
-
-  const setAyatList = (verses: any) => {
-    dispatch(setCurrentAyatNumberList(getAyatNumberList(verses)));
-  };
-
-  const dispatchCurrentMurottal = async () => {
-    if (isMurottalPlaying === true) {
-      dispatch(murottalAudioToggle());
-    }
-    dispatch(setCurrentMurottal(murottalUrl));
-  };
-
-  useEffect(() => {
-    setAyatList(versesArray);
-    dispatchCurrentMurottal();
-  }, [surahId]);
-
-  return (
-    <>
-      <Head>
-        <title>
-          {"Qur'an Surat "} {name_latin || ''}
-          {" | Qur'anKu"}
-        </title>
-        <link rel="preload" href="/fonts/LPMQ.ttf" as="font" crossOrigin="" />
-      </Head>
-
-      <Header
-        pageTitle={
-          name_latin ? `${name_latin} | ${number_of_ayah} ayat` : 'Loading...'
-        }
-      />
-      <main
-        style={{ width: '95%', marginLeft: 'auto', marginRight: 'auto' }}
-        className="my-16"
-      >
-        <ul>
-          <li className="mb-5 border-b pb-3">
-            <div className="flex flex-col">
-              <div className="grid">
-                <p className="arabic-text col-start-2 ml-auto mr-0 text-right">
-                  بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
-                </p>
-              </div>
-              <p className="mt-10 text-justify leading-relaxed text-gray-700">
-                Dengan nama Allah Yang Maha Pengasih, Maha Penyayang.
-              </p>
-            </div>
-          </li>
-          {versesArray.map((ayat) => (
-            <AyatListItem
-              key={`${name_latin}-${ayat.ayatNumber}`}
-              surahId={surahId}
-              surahName={name_latin}
-              {...ayat}
-            />
-          ))}
-        </ul>
-        <audio id="audio-murottal" src={currentMurottal} loop={true} />
-      </main>
-      {number ? (
-        <Footer
-          previousId={(parseInt(number, 10) - 1).toString()}
-          nextId={(parseInt(number, 10) + 1).toString()}
-          previousName={previousName}
-          nextName={nextName}
-        />
-      ) : (
-        <Footer
-          previousId={null}
-          nextId={null}
-          previousName="Loading"
-          nextName="Loading"
-        />
-      )}
-    </>
-  );
+const SurahDetailPage = ({ surahData, murottalUrl }: SurahDetailPageProps) => {
+  return <SurahDetailApp surahData={surahData} murottalUrl={murottalUrl} />;
 };
 
-export default SurahDetail;
+export default SurahDetailPage;
--- types/surah.ts
+++ types/surah.ts
@@ -1,16 +1,3 @@
-export type SurahItem = {
-  arti: string;
-  asma: string;
-  audio: string;
-  ayat: number;
-  keterangan: string;
-  nama: string;
-  nomor: string;
-  rukuk: string;
-  type: string;
-  urut: string;
-};
-
 export interface ISurah {
   arti: string;
   asma: string;
@@ -25,3 +12,43 @@ export interface ISurah {
 }
 
 export type TSurahLinkPrefix = 'surat' | 'tafsir';
+
+export type TTafsir = {
+  id: {
+    kemenag: {
+      name: string;
+      source: string;
+      text: {
+        [k: number]: string;
+      };
+    };
+  };
+};
+
+export type TTranslations = {
+  id: {
+    name: string;
+    text: {
+      [k: number]: string;
+    };
+  };
+};
+
+export interface ISurahDetail {
+  name: string;
+  name_latin: string;
+  number: string;
+  number_of_ayah: string;
+  tafsir: TTafsir;
+  text: {
+    [k: number]: string;
+  };
+  translations: TTranslations;
+}
+
+export interface ISurahMetaData {
+  nextSurahName?: string;
+  previourSurahName?: string;
+  nextSurahNumber?: string;
+  previousSurahNumber?: string;
+}
