83a8e2a9fc073b40f3a5a10ce1ba22605ef29af2
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2026-02-08 23:12:03 +0700

    feat: install GTM (#25)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,14 @@ import '../styles/globals.css';
 // import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import type { AppProps } from 'next/app';
 
+import GoogleTagManager from '@/components/Analytics/GoogleTagManager';
+
 import Store from '../state/Store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Store>
+      <GoogleTagManager />
       <Component {...pageProps} />
     </Store>
   );
