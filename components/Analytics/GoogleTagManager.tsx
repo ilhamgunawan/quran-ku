a8b61f1e7a5683faa9f02760127b4ce547dd68bd
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2026-02-08 23:10:08 +0700

    feat: install GTM

---
+++ components/Analytics/GoogleTagManager.tsx
@@ -0,0 +1,17 @@
+import GoogleTagManagerNoScript from './GoogleTagManagerNoScript';
+import GoogleTagManagerScript from './GoogleTagManagerScript';
+
+const gtmId = process.env.NEXT_PUBLIC_GOOGLE_GTM_ID;
+
+export default function GoogleTagManager() {
+  if (!gtmId) {
+    return null;
+  }
+
+  return (
+    <>
+      <GoogleTagManagerScript gtmId={gtmId} />
+      <GoogleTagManagerNoScript gtmId={gtmId} />
+    </>
+  );
+}
---
+++ components/Analytics/GoogleTagManagerNoScript.tsx
@@ -0,0 +1,12 @@
+export default function GoogleTagManagerNoScript({ gtmId }: { gtmId: string }) {
+  return (
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+        height="0"
+        width="0"
+        style={{ display: 'none', visibility: 'hidden' }}
+      ></iframe>
+    </noscript>
+  );
+}
